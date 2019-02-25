00a3610edc78fef44408cc0a4394b1ffa2b1ea9e
mannycolon <colonmanuel7@gmail.com>
2019-02-24 22:21:56 -0500

    Added graphQL subscription for new votes

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -47,6 +47,33 @@ const NEW_LINKS_SUBSCRIPTION = gql`
   }
 `
 
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
+
 class LinkList extends Component {
   _updateCacheAfterVote = (store, createVote, linkId) => {
     const data = store.readQuery({ query: FEED_QUERY })
@@ -76,6 +103,12 @@ class LinkList extends Component {
     })
   }
 
+  _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    })
+  }
+
   render () {
     return (
       <Query query={FEED_QUERY}>
@@ -84,6 +117,7 @@ class LinkList extends Component {
           if (error) return <div>Error</div>
 
           this._subscribeToNewLinks(subscribeToMore)
+          this._subscribeToNewVotes(subscribeToMore)
 
           const linksToRender = data.feed.links
 
