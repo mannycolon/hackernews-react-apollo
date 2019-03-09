467c27c6cf22eba88f771dd1df5d1ef718d94b82
mannycolon <colonmanuel7@gmail.com>
2019-03-09 18:56:24 -0500

    Created new API operations & moved schema to its own file

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,62 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-// typeDefs defines the GraphQL schema
-// The exclamation mark in the type definition means that this field can never be null.
-const typeDefs = `
- type Query {
-   info: String!
- }
-`
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 
-// 2
+let idCount = links.length
 const resolvers = {
   Query: {
-    info: () => 'This is the API of a Hackernews Clone'
+    info: () => 'This is the API of a Hackernews Clone',
+    feed: () => links,
+    link: (parent, args) => {
+      const link = links.find((link) => link.id === args.id)
+      return link
+    },
+  },
+  Mutation: {
+    post: (parent, args) => {// args: carries the arguments for the operation
+      const link = {
+        id: `link-${idCount++}`,
+        url: args.url,
+        description: args.description,
+      }
+
+      links.push(link)
+
+      return link
+    },
+    updateLink: (parent, args) => {
+      const link = links.find(link => link.id === args.id)
+      link.url = args.url
+      link.description = args.description
+
+      return link
+    },
+    deleteLink: (parent, args) => {
+      const link = links.find(link => link.id === args.id)
+      const index = links.indexOf(link)
+
+      links.splice(index, 1)
+
+      return link
+    }
   }
+  // In any case, because the implementation of the Link resolvers is trivial, 
+  // you can actually omit them and the server will work in the same way as it did before 👌
+  // Link: {
+  //   id: (parent) => parent.id,
+  //   url: (parent) => parent.url,
+  //   description: (parent) => parent.description
+  // }
 }
 
 // 3
 // This tells the server what API operations are accepted and how they should be resolved.
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql', // One convenient thing about the constructor of the GraphQLServer is that typeDefs can be provided either directly as a string or by referencing a file that contains your schema definition 
   resolvers
 })
 
